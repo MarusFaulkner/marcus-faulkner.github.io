4713e96c4c798bc366741b508dd797efaff09087
MarusFaulkner <faulkmar@yahoo.com>
2025-06-26 04:52:14 -0500

    Fix website serving errors

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/',
+  root: '.',
   server: {
     port: 3000,
     host: true
   },
   build: {
-    outDir: 'dist',
-    sourcemap: true
+    outDir: 'dist'
   }
 })
